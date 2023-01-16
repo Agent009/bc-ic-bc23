9685c675b129caf9bc771ccad49575b23a02ed16
Amir <amir@connextar.com>
2023-01-16 23:00:46 +0000

    Renamed local project dir

---
+++ src/bc23_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { bc23_backend } from "../../declarations/bc23_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await bc23_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
